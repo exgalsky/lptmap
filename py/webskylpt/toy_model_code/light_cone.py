5235f0c29269530d0753621b3c3485172eb91a39
Shamik Ghosh <thequarkexpress@gmail.com>
2023-02-17 16:38:20 -0800

    MPI and serial switch implemented and tested

--- py/webskylpt/toy_model_code/light_cone.py
+++ py/webskylpt/toy_model_code/light_cone.py
@@ -3,14 +3,24 @@ import cosmology as cosmo
 import healpy as hp
 import matplotlib.pyplot as plt
 import scipy.constants as cons
-# from joblib import Parallel, delayed
-from mpi4py import MPI
 from time import time
 
+run_with_mpi = False
+if run_with_mpi:
+    try: 
+        from mpi4py import MPI
+    except:
+        print("WARNING: mpi4py not found, fallback to serial implementation.")  # todo: Replace print warning messages with proper logging implementation
+        run_with_mpi = False
+
 # MPI communicator initialization
-comm = MPI.COMM_WORLD
-id = comm.Get_rank()            #number of the process running the code
-numProc = comm.Get_size()       #total number of processes running
+if run_with_mpi:
+    comm = MPI.COMM_WORLD
+    id = comm.Get_rank()            #number of the process running the code
+    numProc = comm.Get_size()       #total number of processes running
+else:
+    id = 0
+    numProc = 1 
 
 t0 = time()
 # Setup cosmology workspace on all processes so that class method functions are initialized
@@ -18,14 +28,16 @@ cosmo_wsp = cosmo.cosmology()
 
 t1 = time()
 
-print("Evalutation of cosmology took", t1-t0, "s for Proc ", id)
-# Cube shape is gridx_sz = gridy_sz = gridz_sz, for now, allowing for later change to different x/y/z sizes
-gridx_sz = 128 #6144
-# gridy_sz = 6144
-# gridz_sz = 6144 
+print("Evaluation of cosmology took", t1-t0, "s for Proc ", id)    # todo: Timing statements should also be implemented with logging module
+# Cube shape is grid_xsize = gridy_sz = gridz_sz, for now, allowing for later change to different x/y/z sizes
+grid_xsize = 128 #6144
+# grid_ysize = 6144
+# grid_zsize = 6144 
+
+L_box = 7700. # in Mpc
 
 # Lattice spacing (a_latt in Websky parlance) in Mpc
-gridsz_in_Mpc = 7700 / 6144  # in Mpc; 7700 Mpc box length for websky 6144 cube  
+gridsz_in_Mpc = L_box / 6144 #grid_xsize  # in Mpc; 7700 Mpc box length for websky 6144 cube  
 
 # comoving distance to last scattering in Mpc
 comov_lastscatter = 13.8 * (cons.giga / cons.mega) # in Mpc
@@ -47,8 +59,8 @@ if id == 0:
 # Slice the cube along x which is the slowest varying axis (C indexing)
 # Note: all binary files for numpy arrays follow C indexing
 
-    xslab_min = gridx_sz // numProc      # Q
-    iter_remains = np.mod(gridx_sz, numProc)    # R 
+    xslab_min = grid_xsize // numProc      # Q
+    iter_remains = np.mod(grid_xsize, numProc)    # R 
 
 #  XSZ = R x (Q + 1) + (P-R) x Q 
     xslab_per_Proc = np.zeros((numProc,), dtype=np.int16)  # P = len(zslab_per_Proc)
@@ -66,7 +78,8 @@ else:
     xslab_per_Proc = None 
 
 # Send copy of xslab_per_Proc to each process.
-xslab_per_Proc = comm.bcast(xslab_per_Proc, root=0)
+if run_with_mpi:
+    xslab_per_Proc = comm.bcast(xslab_per_Proc, root=0)
 
 # Find the start and stop of the x-axis slab for each process
 xslab_start_in_Proc = np.sum(xslab_per_Proc[0:id])    
@@ -75,25 +88,27 @@ xslab_stop_in_Proc = xslab_start_in_Proc + xslab_per_Proc[id]
 # print(id, zslab_start_in_Proc, zslab_stop_in_Proc)
 
 # Compute single precision offset for reading tranche of displacement values
-sp_data_slab_offset = np.sum(xslab_per_Proc[0:id]) * gridx_sz * gridx_sz *  4  # in bytes 
+sp_data_slab_offset = np.sum(xslab_per_Proc[0:id]) * grid_xsize * grid_xsize *  4  # in bytes 
 
 t2 = time()
 
 print("Slab decomposition took", t2-t1, "s for Proc ", id)
 
 # Setup axes for the slab grid
-xaxis = np.arange(xslab_start_in_Proc, xslab_stop_in_Proc, dtype=np.float32)
-yaxis = np.arange(0, gridx_sz, dtype=np.float32)
-zaxis = np.arange(0, gridx_sz, dtype=np.float32) 
+xaxis = np.arange(xslab_start_in_Proc, xslab_stop_in_Proc, dtype=np.int16)
+yaxis = np.arange(0, grid_xsize, dtype=np.int16)
+zaxis = np.arange(0, grid_xsize, dtype=np.int16) 
 
 # Setup meshgrid for the slab 
-grid_qx, grid_qy, grid_qz = np.meshgrid(xaxis, yaxis, zaxis, indexing='ij')
+grid_qx, grid_qy, grid_qz = np.meshgrid(xaxis, yaxis, zaxis, indexing='ij') # 40.5
 # print(grid_qx.shape, grid_qy.shape, grid_qz.shape)
+
+del xaxis, yaxis, zaxis
 
 t3 = time()
 print("Grid setup took", t3-t2, "s for Proc ", id)
 # Lagrangian comoving distance grid for the slab
-comov_q = np.sqrt((grid_qx+ 0.5)**2. + (grid_qy+0.5)**2. + (grid_qz+0.5)**2.) * gridsz_in_Mpc
+comov_q = (np.sqrt((grid_qx+ 0.5)**2. + (grid_qy+0.5)**2. + (grid_qz+0.5)**2.) * gridsz_in_Mpc).astype(np.float32) # 27 
 
 # plt.imshow(comov_q[0,:,:], cmap=plt.cm.Blues, origin='lower')
 # plt.title('Initial comoving distance process = '+str(id))
@@ -104,7 +119,7 @@ t4 = time()
 print("Comoving distance q took", t4-t3, "s for Proc ", id)
 
 # Lagrangian redshift grid for the slab
-redshift_q = cosmo_wsp.comoving_distance2z(comov_q)
+redshift_q = cosmo_wsp.comoving_distance2z(comov_q).astype(np.float32)  # 27
 
 # plt.imshow(redshift_q[0,:,:], cmap=plt.cm.Blues, origin='lower')
 # plt.title('Lagrangian redshift process = '+str(id))
@@ -115,7 +130,7 @@ t5 = time()
 print("Redshift took", t5-t4, "s for Proc ", id)
 
 # Growth factor D grid for the slab
-growthD = cosmo_wsp.growth_factor_D(redshift_q)
+growthD = cosmo_wsp.growth_factor_D(redshift_q).astype(np.float32)  # 27
 
 # plt.imshow(growthD[0,:,:], cmap=plt.cm.Blues, origin='lower')
 # plt.title('Growth factor D process = '+str(id))
@@ -125,28 +140,26 @@ growthD = cosmo_wsp.growth_factor_D(redshift_q)
 t6 = time()
 print("Growth factor took", t6-t5, "s for Proc ", id)
 
-del xaxis, yaxis, zaxis
-
 # Compute Euclidean grids. Cleanup unwanted grids
 
-grid_sx = np.fromfile(path2disp+'sx1_n128_bin', count=xslab_per_Proc[id] * gridx_sz * gridx_sz, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qx.shape) #sx1_7700Mpc_n6144_nb30_nt16
-grid_Xx = grid_qx + growthD * grid_sx
+grid_sx = np.fromfile(path2disp+'sx1_n128_bin', count=xslab_per_Proc[id] * grid_xsize * grid_xsize, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qx.shape) #sx1_7700Mpc_n6144_nb30_nt16
+grid_Xx = (grid_qx + growthD * grid_sx).flatten()
 
 t7 = time()
 print("Displacement x I/O and Eulerian grid x took", t7-t6, "s for Proc ", id)
 
 del grid_qx, grid_sx
 
-grid_sy = np.fromfile(path2disp+'sy1_n128_bin', count=xslab_per_Proc[id] * gridx_sz * gridx_sz, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qy.shape) #sy1_7700Mpc_n6144_nb30_nt16
-grid_Xy = grid_qy + growthD * grid_sy
+grid_sy = np.fromfile(path2disp+'sy1_n128_bin', count=xslab_per_Proc[id] * grid_xsize * grid_xsize, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qy.shape) #sy1_7700Mpc_n6144_nb30_nt16
+grid_Xy =(grid_qy + growthD * grid_sy).flatten()
 
 t8 = time()
 print("Displacement y I/O and Eulerian grid y took", t8-t7, "s for Proc ", id)
 
 del grid_qy, grid_sy
 
-grid_sz = np.fromfile(path2disp+'sz1_n128_bin', count=xslab_per_Proc[id] * gridx_sz * gridx_sz, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qz.shape) #sz1_7700Mpc_n6144_nb30_nt16
-grid_Xz = grid_qz + growthD * grid_sz
+grid_sz = np.fromfile(path2disp+'sz1_n128_bin', count=xslab_per_Proc[id] * grid_xsize * grid_xsize, offset=sp_data_slab_offset, dtype=np.float32).reshape(grid_qz.shape) #sz1_7700Mpc_n6144_nb30_nt16
+grid_Xz = (grid_qz + growthD * grid_sz).flatten()
 
 t9 = time()
 print("Displacement z I/O and Eulerian grid z took", t9-t8, "s for Proc ", id)
@@ -162,8 +175,10 @@ del growthD
 # plt.savefig('./comov_X_proc'+str(id)+'.png')
 
 # Compute healpix pixel grid from Euclidean x, y, z values
-ipix_grid = hp.vec2pix(nside, grid_Xx.flatten(), grid_Xy.flatten(), grid_Xz.flatten())
-ipix_grid = ipix_grid.reshape((xslab_per_Proc[id], gridx_sz, gridx_sz))
+ipix_grid = hp.vec2pix(nside, grid_Xx, grid_Xy, grid_Xz)
+ipix_grid = ipix_grid.reshape((xslab_per_Proc[id], grid_xsize, grid_xsize))
+
+del grid_Xx, grid_Xy, grid_Xz
 
 t10 = time()
 print("HPX pixel grid took", t10-t9, "s for Proc ", id)
@@ -177,12 +192,15 @@ print("HPX pixel grid took", t10-t9, "s for Proc ", id)
 # W_kappa = a_latt^3 / Omega_pix * (3/2) * Omega_M * (H_0 / c_in_km_per_s)^2 * (1 + z) *(1 - X/X_*) / X
 lensing_kernel_grid = geometric_factor * (3./2.) * cosmo_wsp.params['Omega_m'] * (cosmo_wsp.params['h'] * 100. * cons.kilo / cons.c )**2. * (1 + redshift_q) * (1. - (comov_q/comov_lastscatter)) / comov_q
 
+del redshift_q
 t11 = time()
 print("Lensing kernel grid took", t11-t10, "s for Proc ", id)
-# Mask limiting to constant radius in the cube with origin at (0,0,0) corner.
-# mask_comov = comov_q <= np.max(comov_q[0,0,:])
 
-del redshift_q, comov_q
+# Mask limiting to constant radius in the cube with origin at (0,0,0) corner.
+mask_comov = np.ones(comov_q.shape)
+mask_comov[comov_q > L_box] = 0.
+
+del comov_q
 
 # Find only the unique pixels to compute over
 # ipix_unique = np.unique(ipix_grid)
@@ -199,12 +217,12 @@ del redshift_q, comov_q
 # kappa_values = Parallel(n_jobs=-1, prefer="threads")(delayed (call_LoSinteg)(pix) for pix in ipix_unique)
 
 
-kappa_slab, edges = np.histogram(ipix_grid, bins=npix, range=(-0.5,npix-0.5), weights=lensing_kernel_grid, density=False)
+kappa_slab, edges = np.histogram(ipix_grid, bins=npix, range=(-0.5,npix-0.5), weights=lensing_kernel_grid*mask_comov, density=False)
 
 t12 = time()
 print("LoS integral by histogram took", t7-t6, "s for Proc ", id)
 
-del lensing_kernel_grid, edges#, mask_comov
+del lensing_kernel_grid, edges, mask_comov
 
 # Map the part LoS integrals to relevant pixels 
 # kappa_slab = np.zeros(hp.nside2npix(nside))
@@ -213,7 +231,10 @@ del lensing_kernel_grid, edges#, mask_comov
 # del kappa_values, ipix_unique
 
 # Combine intergals for slabs with reduction
-comm.Reduce([kappa_slab, MPI.DOUBLE], kappa, op=MPI.SUM, root=0)
+if run_with_mpi:
+    comm.Reduce([kappa_slab, MPI.DOUBLE], kappa, op=MPI.SUM, root=0)
+else:
+    kappa = kappa_slab
 
 t13 = time()
 print("Reduce kappa took", t13-t12, "s for Proc ", id)
@@ -223,10 +244,10 @@ if id == 0:
     t14 = time()
     print("Job completion took", t14-t0, "s for Proc ", id)
     # Save map and plot figure:
-    hp.write_map('./kappa-map_websky1lpt_nside'+str(nside)+'_MPI.fits', kappa, dtype=np.float64, overwrite=True)
+    # hp.write_map('../output/kappa-map_websky1lpt_no768_nside'+str(nside)+'_MPI.fits', kappa, dtype=np.float64, overwrite=True)
     hp.orthview(kappa, rot=[0.,90.,0.], cmap='inferno', half_sky=True, title=r'$\kappa$ map')
     hp.graticule(ls='-', lw=0.25, c='w')
-    plt.savefig('./kappa_map_smallcube.png', bbox_inches='tight', pad_inches=0.1)
+    plt.savefig('./kappa_small_cube_128.png', bbox_inches='tight', pad_inches=0.1) #../output/kappa_map_fullcube_no768.png
 
 
 
