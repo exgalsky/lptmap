b9a112786391194b48aede6918aad521c78072b3
Shamik Ghosh <thequarkexpress@gmail.com>
2023-06-01 20:52:21 -0700

    add files to resolve rebase conflict

--- py/lightcone/jax_implementation/jax_cone.py
+++ py/lightcone/jax_implementation/jax_cone.py
@@ -36,9 +36,7 @@ t0 = time()
 # Setup cosmology workspace on all processes so that class method functions are initialized
 cosmo_wsp = cosmo.cosmology()
 
-t1 = time()
-
-print("Evalutation of cosmology took", t1-t0, "s ")
+t1 = time() ; print("Evalutation of cosmology took", t1-t0, "s ")
 
 # Lattice spacing (a_latt in Websky parlance) in Mpc
 lattice_size_in_Mpc = L_box / grid_nside  # in Mpc; 7700 Mpc box length for websky 6144 cube  
@@ -59,103 +57,132 @@ solidang_pix = 4*np.pi / npix
 # Effectively \Delta chi, comoving distance interval spacing for LoS integral
 geometric_factor = lattice_size_in_Mpc**3. / solidang_pix
 
-t2 = time()
+
 # Setup axes for the slab grid
 xaxis = jnp.arange(0, grid_nside, dtype=jnp.int16)
-yaxis = jnp.arange(0, grid_nside, dtype=jnp.int16) 
-zaxis = jnp.arange(0, grid_nside, dtype=jnp.int16) 
+yaxis = jnp.arange(0, grid_nside, dtype=jnp.int16)
+zaxis = jnp.arange(0, grid_nside, dtype=jnp.int16)
 
 # Setup meshgrid for the slab 
-grid_qx, grid_qy, grid_qz = jnp.meshgrid(xaxis, yaxis, zaxis, indexing='ij')
+# grid_qx, grid_qy, grid_qz = jnp.meshgrid(xaxis, yaxis, zaxis, indexing='ij')        # 6 : 6
 
-del xaxis, yaxis, zaxis
+# del xaxis, yaxis, zaxis
 
-skymap = np.zeros((npix,))
+skymap = np.zeros((npix,))    
 shift_param = grid_nside
 origin_shift = [(0,0,0), (-shift_param,0,0), (0,-shift_param,0), (-shift_param,-shift_param,0),
                 (0,0,-shift_param), (-shift_param,0,-shift_param), (0,-shift_param,-shift_param), (-shift_param,-shift_param,-shift_param)]
-t3 = time()
-print("Grid setup took", t3-t2, "s ")
+t2 = time() ; print("Initial setup took", t2-t1, "s ")
 
 # Lagrangian comoving distance grid for the slab
+@partial(jax.jit, static_argnames=['trans_vec', 'Dgrid_in_Mpc'])
+def lagrange_mesh(x_axis, y_axis, z_axis, trans_vec, Dgrid_in_Mpc):
+    return jnp.meshgrid( jnp.float32((x_axis + 0.5 + trans_vec[0]) * Dgrid_in_Mpc), jnp.float32((y_axis + 0.5 + trans_vec[1]) * Dgrid_in_Mpc), jnp.float32((z_axis + 0.5 + trans_vec[2]) * Dgrid_in_Mpc), indexing='ij')
 
 @partial(jax.jit, static_argnames=['trans_vec', 'Dgrid_in_Mpc'])
 def comoving_q(x_i, y_i, z_i, trans_vec, Dgrid_in_Mpc):
-    return jnp.sqrt((x_i + 0.5 + trans_vec[0])**2. + (y_i + 0.5 + trans_vec[1])**2. + (z_i + 0.5 + trans_vec[2])**2.) * Dgrid_in_Mpc
+    return jnp.sqrt(x_i**2. + y_i**2. + z_i**2.)
+    # return jnp.sqrt((x_i + 0.5 + trans_vec[0])**2. + (y_i + 0.5 + trans_vec[1])**2. + (z_i + 0.5 + trans_vec[2])**2.) * Dgrid_in_Mpc
 
 @partial(jax.jit, static_argnames=['Dgrid_in_Mpc', 'trans'])
 def euclid_i(q_i, s_i, growth_i, Dgrid_in_Mpc, trans):
+<<<<<<< HEAD
     return (q_i + 0.5 + trans) * Dgrid_in_Mpc + growth_i * s_i
+=======
+    return q_i + growth_i * s_i 
+    # return q_i * Dgrid_in_Mpc + growth_i * s_i + 0.5 + trans*Dgrid_in_Mpc
+>>>>>>> 564b11c (Memory profile for jax_cone)
 
 @jax.jit
 def lensing_kernel_F(comov_q_i, redshift_i):
     return geometric_factor * (3./2.) * cosmo_wsp.params['Omega_m'] * (cosmo_wsp.params['h'] * 100. * cons.kilo / cons.c )**2. * (1 + redshift_i) * (1. - (comov_q_i/comov_lastscatter)) / comov_q_i
 
 def read_displacement(filename):
-    return jnp.asarray(np.fromfile(filename, count=grid_nside * grid_nside * grid_nside, dtype=jnp.float32).reshape(grid_qx.shape), dtype=jnp.float32)
+    return jnp.asarray(np.fromfile(filename, count=grid_nside * grid_nside * grid_nside, dtype=jnp.float32).reshape((grid_nside, grid_nside, grid_nside)), dtype=jnp.float32)
 
-t3b = time()
-print("Jit compilation took", t3b-t3, "s ")
-t3 = t3b
+t3 = time() ; print("Jit compilation took", t3-t2, "s ")
+
 
 store_displacements=True
 if store_displacements:
-    grid_sx = read_displacement(sxfile)
-    grid_sy = read_displacement(syfile)
-    grid_sz = read_displacement(szfile)
+    grid_sx = read_displacement(sxfile)     # 4 : 10
+    grid_sy = read_displacement(syfile)     # 4 : 14
+    grid_sz = read_displacement(szfile)     # 4 : 18
 
-t3b = time()
-print("I/O took", t3b-t3, "s ")
-t3 = t3b
+t4 = time() ; print("I/O took", t4-t3, "s ")
 
 for translation in origin_shift:
+
+    t4 = time()
     print(translation)
-    t3_5 = time()
-    lagrange_grid = jax.vmap(comoving_q, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qx, grid_qy, grid_qz, translation, lattice_size_in_Mpc)
 
-    t4 = time() ; print("Lagrange grid took", t4-t3_5, "s ")
-    redshift_grid = jax.vmap(cosmo_wsp.comoving_distance2z)(lagrange_grid)
+    grid_qx, grid_qy, grid_qz = lagrange_mesh(xaxis, yaxis, zaxis, translation, lattice_size_in_Mpc)
 
-    t5 = time() ; print("Redshift took", t5-t4, "s ")
+    t5 = time() ; print("Largrangian meshgrid took", t5 - t4, "s ")
 
-    growth_grid = jax.vmap(cosmo_wsp.growth_factor_D)(redshift_grid)
+    lagrange_grid = jax.vmap(comoving_q, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qx, grid_qy, grid_qz, translation, lattice_size_in_Mpc)    # 4 : 22
 
-    t6 = time() ; print("Growth took", t6-t5, "s ")
+    t6 = time() ; print("Lagrangian comoving distance grid took", t6 - t5, "s ")
+    redshift_grid = jax.vmap(cosmo_wsp.comoving_distance2z)(lagrange_grid)      # 4 : 26
+
+    t7 = time() ; print("Redshift took", t7-t6, "s ")
+    # Compute healpix pixel grid from Lagrangian x, y, z values
+    ipix_grid = jhp.vec2pix(nside, grid_qz, grid_qy, grid_qx)    # 8 : 38
+
+    t8 = time() ; print("HPX pixel grid (Lagrangian) took", t8-t7, "s ")
+
+    kernel_sphere = jnp.where((lagrange_grid >= chimin) & (lagrange_grid <= chimax), jax.vmap(lensing_kernel_F)(lagrange_grid, redshift_grid), 0.)      # 4 : 42
+
+    t9 = time() ; print("Kernel grid (Lagrangian) took", t9-t8, "s ")
+
+    skymap += np.asarray(jnp.histogram(ipix_grid, bins=npix, range=(-0.5,npix-0.5), weights=-kernel_sphere, density=False)[0])      
+    del kernel_sphere, ipix_grid         # -20 : 22
+
+    t10 = time() ; print("Project to healpix (Lagrangian) took", t10-t9, "s ")
+
+    growth_grid = jax.vmap(cosmo_wsp.growth_factor_D)(redshift_grid)        #   4 : 30
+
+    t11 = time() ; print("Growth took", t11-t10, "s ")
 
     if not store_displacements: grid_sx = read_displacement(sxfile)
-    grid_Xx = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qx, grid_sx, growth_grid, lattice_size_in_Mpc, translation[0])
+    grid_Xx = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qx, grid_sx, growth_grid, lattice_size_in_Mpc, translation[0])     # 4 : 34
+    del grid_qx
     if not store_displacements: del grid_sx
 
     if not store_displacements: grid_sy = read_displacement(syfile)
-    grid_Xy = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qy, grid_sy, growth_grid, lattice_size_in_Mpc, translation[1])
+    grid_Xy = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qy, grid_sy, growth_grid, lattice_size_in_Mpc, translation[1])     # 4 : 38
+    del grid_qy
     if not store_displacements: del grid_sy
 
     if not store_displacements: grid_sz = read_displacement(szfile)
-    grid_Xz = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qz, grid_sz, growth_grid, lattice_size_in_Mpc, translation[2])
+    grid_Xz = jax.vmap(euclid_i, in_axes=(0, 0, 0, None, None), out_axes=0)(grid_qz, grid_sz, growth_grid, lattice_size_in_Mpc, translation[2])     # 4 : 42
+    del grid_qz, growth_grid
     if not store_displacements: del grid_sz
 
-    t7 = time() ; print("Displacements took", t7-t6, "s ")
+    t12 = time() ; print("Displacements took", t12-t11, "s ")
 
     # Compute healpix pixel grid from Euclidean x, y, z values
-    ipix_grid = jhp.vec2pix(nside, grid_Xz, grid_Xy, grid_Xx)
-    del grid_Xx, grid_Xy, grid_Xz
+    ipix_grid = jhp.vec2pix(nside, grid_Xz, grid_Xy, grid_Xx)   # 8 : 50 
+    del grid_Xx, grid_Xy, grid_Xz               # -12 : 38
 
-    t8 = time() ; print("HPX pixel grid (Eulerian) took", t8-t7, "s ")
+    t13 = time() ; print("HPX pixel grid (Eulerian) took", t13-t12, "s ")
 
-    kernel_sphere = jnp.where((lagrange_grid >= chimin) & (lagrange_grid <= chimax), jax.vmap(lensing_kernel_F)(lagrange_grid, redshift_grid), 0.)
+    kernel_sphere = jnp.where((lagrange_grid >= chimin) & (lagrange_grid <= chimax), jax.vmap(lensing_kernel_F)(lagrange_grid, redshift_grid), 0.)      # 4 : 42
+    del lagrange_grid, redshift_grid
 
-    t9 = time() ; print("Kernel grid (Eulerian) took", t9-t8, "s ")
+    t14 = time() ; print("Kernel grid (Eulerian) took", t14-t13, "s ")
 
     skymap += np.asarray(jnp.histogram(ipix_grid, bins=npix, range=(-0.5,npix-0.5), weights=kernel_sphere, density=False)[0])
-    del ipix_grid, kernel_sphere
+    del ipix_grid, kernel_sphere           # -12 : 30
 
-    t10 = time() ; print("Project to healpix (Eulerian) took", t10-t9, "s ")
+    t15 = time() ; print("Project to healpix (Eulerian) took", t15-t14, "s ")
 
-    # Compute healpix pixel grid from Lagrangian x, y, z values
-    ipix_grid = jhp.vec2pix(nside, (grid_qz + 0.5 + translation[2])*lattice_size_in_Mpc, (grid_qy + 0.5 + translation[1])*lattice_size_in_Mpc, (grid_qx + 0.5 + translation[0])*lattice_size_in_Mpc)
+    exit()
+if store_displacements: del grid_sx, grid_sy, grid_sz
 
-    t11 = time() ; print("HPX pixel grid (Lagrangian) took", t11-t10, "s ")
+# del grid_qx, grid_qy, grid_qz
 
+<<<<<<< HEAD
     kernel_sphere = jnp.where((lagrange_grid >= chimin) & (lagrange_grid <= chimax), jax.vmap(lensing_kernel_F)(lagrange_grid, redshift_grid), 0.)
 
     t12 = time() ; print("Kernel grid (Lagrangian) took", t12-t11, "s ")
@@ -169,6 +196,9 @@ for translation in origin_shift:
 del grid_qx, grid_qy, grid_qz
 
 print("Job completion took", t13-t0, "s ")
+=======
+print("Job completion took", t15-t0, "s ")
+>>>>>>> 564b11c (Memory profile for jax_cone)
 # Save map and plot figure:
 hp.write_map('./output/kappa-map_websky1lpt_nside'+str(nside)+'_768.fits', skymap, dtype=np.float64, overwrite=True)
 fig = plt.figure(figsize=(6,4), dpi=600)
